54737deb605e59bbb0bfaacbc95c8fcfa9290184
Toshihiro Yagi <toshihiro-yagi@cookpad.com>
2014-09-19 16:33:51 +0900

    Add genymotion_peco.sh

---
+++ bin/genymotion_peco.sh
@@ -0,0 +1,18 @@
+genymotion_peco(){
+
+  if [ -z "$GENYMOTION_APP_HOME" ]; then
+    echo "GENYMOTION_APP_HOME is empty. Use '/Applications/Genymotion.app/' instead this time."
+    player="/Applications/Genymotion.app/Contents/MacOS/player"
+  else
+    player="$GENYMOTION_APP_HOME/Contents/MacOS/player"
+  fi
+
+  vm_name=`VBoxManage list vms | peco`
+  if [[ $vm_name =~ ^\"(.+)\".* ]] ; then
+     name=${BASH_REMATCH[1]}
+     echo "boot $name"
+     $player --vm-name "$name"
+  fi
+
+}
+
